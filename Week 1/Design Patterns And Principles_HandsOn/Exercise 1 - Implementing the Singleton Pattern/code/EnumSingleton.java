2a02b76d7a82a7cf35f3eb37bb65de3093b80973
RJ-MONIKA <monikarj.22aid@kongu.edu>
2025-06-21 21:46:05 +0530

    singleton modified

--- Week 1/Design Patterns And Principles_HandsOn/Exercise 1 - Implementing the Singleton Pattern/code/EnumSingleton.java
+++
@@ -1,9 +0,0 @@
-package designpatterns.singleton;
-
-public enum EnumSingleton {
-    INSTANCE;
-
-    public void show() {
-        System.out.println("Enum Singleton Instance Accessed");
-    }
-}
